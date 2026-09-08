1082b82898b6be04e70a13191002eb4bf734c3b1
ArrogHie <ArrogHie@gmail.com>
2026-09-08 21:16:17 +0800

    chore: prepare release v1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.arroghie.xmutimetable"
         minSdk = 26
         targetSdk = 36
-        versionCode = 102
-        versionName = "1.1.1"
+        versionCode = 103
+        versionName = "1.1.2"
         vectorDrawables { useSupportLibrary = true }
         androidResources {
             localeFilters += listOf("zh-rCN", "zh-rTW", "en", "ja", "es")
